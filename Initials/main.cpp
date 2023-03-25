db2a7519ea182852390592cb8360790f01a29d4b
StijnClara <87970313+StijnClara@users.noreply.github.com>
2023-03-25 07:53:39 +0100

    Initials

---
+++ Initials/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << " SSS    CCCC\n"
+                 "S   S  C\n"
+                 "S      C\n"
+                 " SSS   C\n"
+                 "    S  C\n"
+                 "S   S  C\n"
+                 " SSS    CCCC" << std::endl;
+    return 0;
+}
