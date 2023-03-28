fa60786ab48ee558fb1cf99c61f128c8bae50433
StijnClara <87970313+StijnClara@users.noreply.github.com>
2023-03-28 18:36:10 +0200

    Tree

---
+++ Tree/src/Tree.java
@@ -0,0 +1,13 @@
+public class Tree {
+    public static void main(String[] args) {
+        // Building a tree
+        System.out.println("Hey there, I’m Stijn and I’m learning to code in Java!");
+        System.out.println("I’m going to plant a tree today!");
+        System.out.println("  *  \n" +
+                " *** \n" +
+                " *** \n" +
+                "  *  \n" +
+                "  *    ");
+
+    }
+}
