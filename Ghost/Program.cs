f6c054c7411dab0509da65520eaa405b10005234
StijnClara <87970313+StijnClara@users.noreply.github.com>
2023-07-05 14:00:17 +0200

    Ghost

---
+++ Ghost/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(" .-.");    
+        Console.WriteLine("(o o)");
+        Console.WriteLine("|   |");
+        Console.WriteLine("'~~~'");
+    }
+}
