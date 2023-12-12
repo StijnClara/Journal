a0a64e75fa372842e48a5311d7b7cefff8875eb1
StijnClara <87970313+StijnClara@users.noreply.github.com>
2023-12-12 15:14:57 +0100

    Create Money Maker.cs

---
+++ Money Maker.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace MoneyMaker
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+
+
+      Console.WriteLine("Welcome to Money Maker!");
+      Console.WriteLine("Enter an amount to convert to coins: ");
+      string totalAsString = Console.ReadLine();
+      double total = Convert.ToDouble(totalAsString);
+      Console.WriteLine($"{total} cents is equal to...");
+
+      int goldValue = 10;
+      int silverValue = 5;
+      int bronzeValue = 1;
+
+      double goldCoins = Math.Floor(total / goldValue);
+      double leftOver = Math.Floor(total % goldValue);
+      double silverCoins = Math.Floor(leftOver / silverValue);
+      leftOver = Math.Floor(leftOver % silverValue);
+      double bronzeCoins = Math.Floor(leftOver / bronzeValue);
+
+      Console.WriteLine($"Than you get, {goldCoins} gold coins, {silverCoins} silver coins and {bronzeCoins} bronze coins.");
+    }
+  }
+}
